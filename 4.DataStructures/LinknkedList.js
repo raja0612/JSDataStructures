33a9c7794d82ce325b9180e0d172712a8fd51cad
raja0612 <rajashekhar.ramayampeta@gmail.com>
2021-01-17 17:51:55 -0500

    completed single linked list.

--- 4.DataStructures/LinknkedList.js
+++ 4.DataStructures/LinknkedList.js
@@ -5,36 +5,29 @@ class Node {
     }
 }
 
-/* let head = new Node(1);
-head.next = new Node(2);
-head.next.next = new Node(3);
-
-console.log(head);
-console.log(head.next);
-console.log(head.next.next);
-*/
-
-class SinglyLinkedList {
+class LinkedList {
     constructor() {
         this.head = null;
         this.tail = null;
         this.length = 0;
     }
 
+    // inserting val at the end
     push(val) {
-        let newNode = new Node(val);
-       
-        if(!this.head) {
-            this.head = newNode;
-            this.tail = newNode; 
-        } else {
-            this.tail.next = newNode;
-            this.tail = newNode;
-        }
-        this.length++;
-        return this;
-    }
+     let newNode  = new Node(val);
 
+     if(!this.head){
+         this.head = newNode;
+         this.tail = newNode;
+     } else {
+         this.tail.next = newNode;
+         this.tail = newNode;
+     }
+     this.length++;
+     return this;
+    }
+   
+    // print all elements
     traverse() {
         let current = this.head;
         while(current) {
@@ -43,8 +36,9 @@ class SinglyLinkedList {
         }
     }
 
+    // remove node from end(tail)
     pop() {
-        if(!this.head) return undefined;
+        if(!this.head)  return undefined;
         let current = this.head;
         let newTail = current;
 
@@ -55,24 +49,151 @@ class SinglyLinkedList {
         this.tail = newTail;
         this.tail.next = null;
         this.length--;
-        if(this.length === 0) {
+        if(this.head == this.tail) { //if(this.length === 0 )
             this.head = null;
             this.tail = null;
         }
         return current;
     }
+
+    //shift-- remove head element
+    shift() {
+        if(!this.head) return undefined;
+        let currentHead = this.head;
+        this.head = currentHead.next;
+        this.length--;
+        if(this.length === 0){
+            this.tail = null;
+        }
+        return currentHead;
+    }
+
+    //unshift -- add node to begining of list
+    unshift(val) {
+        let newHead = new Node(val);
+        if(!this.head) {
+            this.head = newHead;
+            this.tail = this.head;
+        } else {
+            newHead.next = this.head;
+            this.head = newHead;
+        }
+        this.length++;
+        return this;
+    }
+   // get node from index is being passed
+    get(index) {
+        if(index < 0 || index >= this.length) return undefined;
+        let current = this.head;
+        for(let i = 0; i <= index; i++) {
+            current = current.next;
+        }
+        return current;
+    }
+
+    // set value at given index
+    set(index, val) {
+       let foundNode =  this.get(index);
+
+       if(foundNode) {
+           foundNode.val = val;
+           return true
+       }
+       return false;
+    }
+
+   insert(index, val) {
+       if(index < 0 || index > this.length) return undefined;
+   
+       if(index === this.length) {
+            this.push(val);
+            return true;
+       };
+       if(index === 0) return !!this.unshift(val);
+
+
+       let newNode = new Node(val);
+
+       let prev = this.get(index -1);
+       let next = prev.next;
+       prev.next = newNode;
+       newNode.next = next;
+       this.length++;
+       return true;
+   }
+
+   remove(index) {
+    if(index < 0 || index > this.length) return undefined;
+   
+    if(index === this.length-1) !!this.pop();
+    if(index === 0) return !!this.shift();
+    let prev = this.get(index - 1);
+    let removed = prev.next;
+    prev.next = removed.next;
+    this.length--;
+    return removed;
+   }
+
+   print() {
+       let arr = [];
+       let current = this.head;
+       while(current) {
+           arr.push(current.val);
+           current = current.next;
+       }
+       console.log(arr);
+   }
+
+   reverse() {
+       /*
+           1. swap the head and tail
+           2. create a varibale claled next
+           3. create a varibale claled prev
+           4. ceate a variable called current and intialize it to the head property.
+           5. loop through the list
+           6.  assign prev to current, current to next
+       */
+        
+      let node = this.head;
+      this.head = this.tail;
+      this.tail = node;
+
+      let prev = null;
+      let next;
+
+      for(let i = 0; i < this.length; i++) {
+          next = node.next;
+          node.next = prev;
+          prev = node;
+          node = next;
+      }
+      return this;
+   }
 }
 
+let list = new LinkedList();
+list.push(1);
+list.push(2);
+list.push(3);
+console.log("***********AFTER PUSH******************");
+console.log(list);
+console.log("*************TRAVERSE************")
+list.traverse();
+console.log("*************POP************")
+console.log(list.pop());
+console.log(list.traverse());
+console.log("*************SHIFT************")
+console.log(list.shift());
+console.log(list.unshift(100));
+console.log(list.get(0));
+console.log(list.set(0, 300));
+console.log(list.get(0));
+console.log(list.insert(0, 959));
+console.log(list.insert(2, 1000));
+console.log(list.remove(2));
 
-let linkedList = new SinglyLinkedList();
-linkedList.push(1);
-linkedList.push(2);
-linkedList.push(3);
-console.log(linkedList);
-linkedList.traverse();
-
-console.log(linkedList.pop());
-console.log(linkedList.pop());
-console.log(linkedList.pop());
-console.log(linkedList.pop());
-console.log(linkedList);
+console.log("***********BEFORE REVERSE ******************");
+console.log(list.print());
+console.log("***********AFTER REVERSE ******************");
+console.log(list.reverse());
+console.log(list.print());
