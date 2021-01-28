503f48ad7a281fb97945c90e3cfe488d7710e9c4
raja0612 <rajashekhar.ramayampeta@gmail.com>
2021-01-27 21:38:46 -0500

    completed Queues

--- 4.DataStructures/QueueWithLinkedList.js
+++ 4.DataStructures/QueueWithLinkedList.js
@@ -1,5 +1,5 @@
 class Node {
-    constructor() {
+    constructor(val) {
         this.val = val;
         this.next = null;
     }
@@ -13,11 +13,40 @@ class Queue {
         this.size = 0;
     }
 
+    // adding elements from begining 1->2->3
     enqueue(val) {
-
+        var newNode  = new Node(val);
+        if(!this.first) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+     return ++this.size;
     }
 
+    //removing from begining
     dequeue() {
-        
+        if(!this.first) return null;
+        var temp = this.first;
+        if(this.first === this.last) {
+            this.last = null;
+        }
+        this.first = this.first.next;
+        return temp.val;
     }
 }
+
+
+/*
+
+BIG O of QUEUE
+
+
+Insertion - O(1)
+Removal - O(1)
+Searching - O(N)
+Access - O(N)
+
+*/
