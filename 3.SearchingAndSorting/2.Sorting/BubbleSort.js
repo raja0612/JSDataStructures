974c46abd599a596a75db8f7f464f7a4cc008e56
raja0612 <rajashekhar.ramayampeta@gmail.com>
2021-03-22 21:48:48 -0400

    completed Bubble sort

---
+++ 3.SearchingAndSorting/2.Sorting/BubbleSort.js
@@ -0,0 +1,63 @@
+/*
+
+In each iteration, we comapre two adjacent elements,
+if first element is greather than seconf element, we swap.
+
+In each iteration, the each next max value is placed at end of the array
+and number of comparsions decrease.
+
+*/
+
+function bubbleSort(arr) {
+    const swap = (arr, idx1, idx2) => {
+      [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+    };
+  
+    for (let i = arr.length; i > 0; i--) {
+      for (let j = 0; j < i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+          swap(arr, j, j + 1);
+        }
+      }
+      console.log("ONE PASS COMPLETES");
+    }
+    return arr;
+  }
+
+function bubbleSortOptimization(arr) {
+    var noSwaps;
+
+    const swap = (arr, idx1, idx2) => {
+        [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+    };
+
+    for(let i = arr.length; i > 0; i--) {
+        noSwaps = true;
+        for(let j = 0; j < i-1; j++) {
+            if(arr[j] > arr[j+1]) {
+                swap(arr, j, j+1);
+                noSwaps = false;
+            }
+        }
+        if(noSwaps) break;
+    }
+    return arr;
+}
+
+
+console.log(bubbleSort([15, 38, 1, 42, 2, 19]));
+
+
+console.log(bubbleSortOptimization([8, 1, 2, 3, 4, 5, 6, 7]));
+
+
+/*
+
+Time BIG(O) Complexity
+
+Worst and Average case : O(n2)
+
+
+
+
+*/
---
+++ 3.SearchingAndSorting/2.Sorting/DefaultSort.js
@@ -0,0 +1,10 @@
+
+
+
+console.log(["a", "d", "c", "aa"].sort());
+
+
+function numComparator(a, b) {
+    return a - b;
+}
+console.log([15, 38, 1, 42, 2].sort(numComparator));
