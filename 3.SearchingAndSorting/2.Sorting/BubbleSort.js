2cacba8a0713d63d7fb76de35fec3d0b53d1cf9d
raja0612 <rajashekhar.ramayampeta@gmail.com>
2021-03-22 22:00:24 -0400

    completed bubble sort

--- 3.SearchingAndSorting/2.Sorting/BubbleSort.js
+++ 3.SearchingAndSorting/2.Sorting/BubbleSort.js
@@ -15,6 +15,7 @@ function bubbleSort(arr) {
   
     for (let i = arr.length; i > 0; i--) {
       for (let j = 0; j < i - 1; j++) {
+        console.log(arr, arr[j], arr[j+1]);
         if (arr[j] > arr[j + 1]) {
           swap(arr, j, j + 1);
         }
