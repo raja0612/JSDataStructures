d1dbee48e5dcc02dec84c6ac04318c60281274bb
raja0612 <rajashekhar.ramayampeta@gmail.com>
2021-03-28 21:19:19 -0400

    callbacks completed

--- ES2015/Callbacks.js
+++ ES2015/Callbacks.js
@@ -30,16 +30,91 @@ add(10, 20, (result) => {
 
 
 // asychrnous actions : actions that we initiate now but they finish later (ex: setTimeout, loading scripts and modules);
-function loadScript(src, callback) {
-    let script = document.createElement('script');
-    script.src = src;
-    script.onload = () => callback(script);
-    document.head.append(script);
-  }
-  
-  loadScript('https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.2.0/lodash.js', script => {
-    alert(`Cool, the script ${script.src} is loaded`);
-    alert( _ ); // function declared in the loaded script
-  });
 
-console.log("This line is exexutedbefore loadScript() completes");
+
+
+// synchronous call backs
+
+function isEven(num) {
+  return num % 2 === 0
+}
+
+console.log([1, 2, 3, 4].filter(isEven)); 
+
+// here isEven is callback functon, which is executed after filter function is executed
+
+
+
+// asynchrnous call backs
+
+function download(url, callback) {
+  setTimeout( () => {
+    console.log(`Downloading image ${url}`);
+    callback(url);
+  }, 3 * 1000)
+}
+
+
+download("www.google.com/image", function(url) {
+  console.log(`processing image ${url}`);
+});
+
+// Handling errors
+
+function download2(url, success, failure) {
+  setTimeout( () => {
+    console.log(`Downloading image ${url}`);
+    let error = url.length === 0 || !url;
+    error ? failure("error") : success("success");
+  },3 * 1000)}
+
+download2("",function(res){
+  console.log(`${res}`);
+}, 
+function(err) {
+  console.log(`${err}`);
+})
+
+//Nesting callbacks and the Pyramid of Doom
+
+
+function download3(url, callback) {
+  setTimeout( () => {
+    console.log(`Downloading ${url}`);
+    callback(url);
+  }, 3 * 1000)
+}
+
+
+download3("url1", function(url) {
+  console.log("Processing "+url);
+
+  download3("url2", function(url) {
+    console.log("Processing "+url);
+
+    download3("url3", function(url) {
+      console.log("Processing "+url);
+    })
+  })
+})
+
+// Nesting many asynchronous functions inside callbacks is known as the pyramid of doom or the callback hell:
+/*
+
+asyncFunction(function(){
+  asyncFunction(function(){
+      asyncFunction(function(){
+          asyncFunction(function(){
+              asyncFunction(function(){
+                  ....
+              });
+          });
+      });
+  });
+});
+
+*/
+
+//NOTE : To avoid the pyramid of doom, you use promises or async/await functions.
+
+
